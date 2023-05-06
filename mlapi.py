eefa0d132721520e7fbe40af0b0fed017c94f6a2
aloukikaditya <aloukikaditya@gmail.com>
2023-05-06 19:41:28 -0400

    first commit

---
+++ mlapi.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle 
+import pandas as pd
+app = FastAPI()
+
+class ScoringItem(BaseModel):
+        
+    YearsAtCompany: float
+    EmployeeSatisfaction: float
+    Position: str
+    Salary: int 
+
+with open("rfmodel.pkl", "rb") as f :
+    model = pickle.load(f)
+    
+
+@app.post("/")
+async def scoring_endpoint(item: ScoringItem):
+
+    return item
